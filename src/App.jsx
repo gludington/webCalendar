413314d62b538670ca1212699b824f2d886144e9
Greg Ludington <Greg.Ludington@ipsoft.com>
2023-09-20 00:30:19 -0400

    move from react-scripts to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createContext, useContext } from "react";
 import { ThemeProvider } from "@emotion/react";
